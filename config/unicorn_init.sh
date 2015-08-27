f43e87b85bc59050063ee25a3db75cb1cfc9ec46
alleny77746 <alleny77746@gmail.com>
2015-08-27 17:01:55 -0400

    startup problem

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -13,7 +13,8 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/greg/workspace/McBlooms
+APP_ROOT=/home/greg/workspace/McBlooms/
+
 
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
