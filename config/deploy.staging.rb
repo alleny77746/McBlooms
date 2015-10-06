c11377e71552d5fd78a2138de0d0c154943f66ef
alleny77746 <alleny77746@gmail.com>
2015-10-06 15:18:10 -0400

    deploy2

--- config/deploy.staging.rb
+++ config/deploy.staging.rb
@@ -4,11 +4,13 @@
 load "config/recipes/disable_robots"
 
 #Server setup
-server "204.225.94.90", :web, :app, :db, :primary => true
+#server "204.225.94.90", :web, :app, :db, :primary => true
+server "192.168.10.2", :web, :app, :db, :primary => true
 
 #DOMAIN (URL for site)
 #STAGING URL must be exact
-set :domain, "mcblooms.anlek.com"
+#set :domain, "mcblooms.anlek.com"
+set :domain, "mcblooms.example.com"
 
 #GIT
 set :branch, "master"
