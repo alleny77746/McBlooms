fc85583f71a893506f0ade95db8e61e7427190ae
alleny77746 <alleny77746@gmail.com>
2015-10-06 16:02:09 -0400

    deploy8

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,8 @@ set :user, 'greg'
 
 set :scm, :git
 # set :repository,  "git@gitlab.anlek.com:#{git_repo}.git"
-set :repository, "git@github.com:alleny77746.git/McBlooms.git"
+set :repository, "git@github.com:alleny77746/McBlooms.git"
+
 
 set :deploy_to, "#{app_path}/#{application}/#{rails_env}"
 set :deploy_via, :remote_cache
